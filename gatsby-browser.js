53d2b86eaaab48045c810a4041092f81fa938abe
brittikbasu <brittikbasu@gmail.com>
2020-05-25 01:21:23 +0530

    hide reference box on about page

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,5 +2,7 @@ export const onRouteUpdate = () => {
     if(window.location.pathname === "/"){
         document.querySelector('.my-4').style.display = "none";
         document.querySelector('header').style.display = "none";
+        document.querySelector('.refs-box').style.display = "none";
+
      }
 }
